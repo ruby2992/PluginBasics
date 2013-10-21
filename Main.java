57a9dbb2c79be1b766e66cb3dd13a18d45e441e0
ruby2992 <rubens.kuball@gmx.de>
2013-10-21 18:02:03 +0200

    Create Main.java
    Das ist die Main!

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onDisable() {
+		System.out.println(null);
+	}
+	
+	public void onEnable() {
+		System.out.println(null);
+	}
+}
